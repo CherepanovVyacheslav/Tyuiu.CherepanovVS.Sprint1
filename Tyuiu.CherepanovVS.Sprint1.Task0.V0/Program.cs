046222ec13eff4a3d100e9a03e21619b874cb3fd
Tyuiu.CherepanovVS@outlook.com <Khel102@mail.ru>
2023-09-26 19:40:15 +0500

    Add project files.

---
+++ Tyuiu.CherepanovVS.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.CherepanovVS.Sprint1.V0.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint1.Task0.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Черепанов В.С. | ПКТб-23-1";
+            Console.WriteLine("##############################################################################");
+            Console.WriteLine("*Спринт1                                                                     *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                           *");
+            Console.WriteLine("*Задание #0                                                                  *");
+            Console.WriteLine("*Вариант #0                                                                  *");
+            Console.WriteLine("*Выполнил: Черепанов Вячеслав Сергеевич| ПКТб-23-1                           *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                    *");
+            Console.WriteLine("*Написать программу, которая вычисляет выражение 6/2/3+1-6/2*3               *");
+            Console.WriteLine("*и печатает результат на экране.                                             *");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* 6/2/3+1-6/2*3                                                              *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+            double x=1;
+            double y=5;
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tyuiu.CherepanovVS.Sprint1.V0.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.CherepanovVS.Sprint1.V0.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint1.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 3;
+            var i = ds.Calculate(x, y);
+            Assert.AreEqual(i, -7);
+
+
+
+        }
+    }
+}

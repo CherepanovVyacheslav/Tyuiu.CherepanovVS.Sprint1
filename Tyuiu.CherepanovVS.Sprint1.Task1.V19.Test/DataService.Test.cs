9dfb8222345c089e99573c167006f451ce13edb8
Tyuiu.CherepanovVS@outlook.com <Khel102@mail.ru>
2023-09-26 21:43:43 +0500

    Sprint1Task1

---
+++ Tyuiu.CherepanovVS.Sprint1.Task1.V19.Test/DataService.Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.CherepanovVS.Sprint1.Task1.V19.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint1.Task1.V19.Test
+{
+    [TestClass]
+    public class DataService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService1 ret = new DataService1();
+            double x = 5.0;
+            double y = 1.0;
+            double i = ret.Calculate(x, y);
+            Assert.AreEqual(i, 4);
+        }
+    }
+}
---
+++ Tyuiu.CherepanovVS.Sprint1.Task1.V19/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.CherepanovVS.Sprint1.Task1.V19.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint1.Task1.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService1 ret = new DataService1();
+
+            Console.Title = "Спринт #1 | Выполнил: Черепанов В.С. | ПКТб-23-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*Спринт1                                                                   *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                         *");
+            Console.WriteLine("*Задание #1                                                                *");
+            Console.WriteLine("*Вариант #19                                                               *");
+            Console.WriteLine("*Выполнил: Черепанов Вячеслав Сергеевич| ПКТб-23-1                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("*Написать программу, которая запрашивает у пользователя исходные данные,   *");
+            Console.WriteLine("*вычисляет результат по формуле (1+3*x)/(4*y) и печатает его на экране.    *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("****************************************************************************");
+            double x, y;
+            Console.WriteLine("Введите значение переменной х");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной y");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine(ret.Calculate(x,y));
+            Console.ReadLine();
+        }
+    }
+}

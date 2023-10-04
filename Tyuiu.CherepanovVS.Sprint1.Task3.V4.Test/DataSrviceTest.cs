37f4088b0df9ee589b714fc1075421b1e542abd2
Tyuiu.CherepanovVS@outlook.com <Khel102@mail.ru>
2023-10-04 21:40:50 +0500

    Task3

---
+++ Tyuiu.CherepanovVS.Sprint1.Task3.V4.Test/DataSrviceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.CherepanovVS.Sprint1.Task3.V4.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint1.Task3.V4.Test
+{
+    [TestClass]
+    public class DataSrviceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double priceNotebook = 2.75;
+            double priceCover = 0.5;
+            int quantity = 7;
+            double wate = 22.75;
+            var res = ds.PurchaseAmount(priceNotebook, priceCover, quantity);
+            Assert.AreEqual(wate, res);
+
+        }
+    }
+}
---
+++ Tyuiu.CherepanovVS.Sprint1.Task3.V4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.CherepanovVS.Sprint1.Task3.V4.Lib;
+
+namespace Tyuiu.CherepanovVS.Sprint1.Task3.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Черепанов В.С. | ПКТб-23-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*Спринт1                                                                   *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                  *");
+            Console.WriteLine("*Задание #3                                                                *");
+            Console.WriteLine("*Вариант #4                                                                *");
+            Console.WriteLine("*Выполнил: Черепанов Вячеслав Сергеевич| ПКТб-23-1                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                  *");
+            Console.WriteLine("*Написать программу, которая запрашивает у пользователя исходные данные,   *");
+            Console.WriteLine("*выполняет указанные расчёты и печатает результат на экране.               *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                          *");
+            Console.WriteLine("****************************************************************************");
+            double priceNotebook = 2.75;
+            Console.WriteLine("Стоимость тетрадок = " + priceNotebook);
+            double priceCover = 0.5;
+            Console.WriteLine("Стоимость обложек = " + priceCover);
+            int quantity = 7;
+            Console.WriteLine("Количество комплектов = " + quantity);
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            var res = ds.PurchaseAmount(priceNotebook, priceCover, quantity);
+            Math.Round(res, 3);
+            Console.WriteLine("Стоимость покупки = " + ds.PurchaseAmount(priceNotebook, priceCover, quantity));
+            Console.ReadLine();
+        }
+    }
+}
